4f3339261799046e7a380c423bb7091e3c6d3146
DanielBenBassat <daniel.benbassat@gmail.com>
2023-11-03 09:38:16 +0200

    server and client initial

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+
+MAX_PACKET = 1024
+IP= '127.0.0.1'
+PORT= 8820
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+
+    my_socket.connect((IP, PORT))
+
+    my_socket.send('hello world'.encode())
+
+    response = my_socket.recv(MAX_PACKET).decode()
+
+
+except socket.error as err:
+
+    print('received socket error ' + str(err))
+
+finally:
+
+    my_socket.close()
