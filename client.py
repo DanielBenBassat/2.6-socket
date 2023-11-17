e55dcd0286d44eb3482abebc8bfacfe90d2f1bd8
DanielBenBassat <daniel.benbassat@gmail.com>
2023-11-17 09:38:30 +0200

    adding protocol

--- client.py
+++ client.py
@@ -1,31 +1,48 @@
 import socket
 
 MAX_PACKET = 1024
-IP= '127.0.0.1'
+IP = '127.0.0.1'
 PORT = 1234
 
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
-    my_socket.connect((IP, PORT))
-    check= True
-    while check:
-
-        func= input("enter a func")
-        my_socket.send(func.encode())
-        if func != "EXIT":
-            response = my_socket.recv(MAX_PACKET).decode()
-            print (response)
-        elif func == "EXIT":
-            check= False
+def protocol_send(message):
+    msg_len = len(message)
+    final_msg = str(msg_len) + '!' + message
+    return final_msg
 
 
+def recieve_len_protocol(my_socket):
+    current_char = ''
+    len = ""
+    current_char= my_socket.recv(1).decode()
+    while current_char != '!':
+        len += current_char
+        current_char = my_socket.recv(1).decode()
+    return my_socket.recv(int(len)).decode()
 
 
-except socket.error as err:
+def main():
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        my_socket.connect((IP, PORT))
+        check = True
+        while check:
+            func = input("enter a func")
+            my_socket.send(protocol_send(func).encode())
+            print ("msg sent")
+            if func != "EXIT":
+                response = recieve_len_protocol(my_socket)
+                print(response)
+            elif func == "EXIT":
+                check = False
 
-    print('received socket error ' + str(err))
+    except socket.error as err:
+        print('received socket error ' + str(err))
 
-finally:
+    finally:
+        print("client left the server")
+        my_socket.close()
 
-    my_socket.close()
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -7,59 +7,76 @@ IP = '0.0.0.0'
 PORT = 1234
 QUEUE_LEN = 1
 
+
+def protocol_send(message):
+    msg_len = len(message)
+    final_msg = str(msg_len) + '!' + message
+    return final_msg
+
+
+def recieve_len_protocol(my_socket):
+    current_char = ''
+    len = ""
+    current_char = my_socket.recv(1).decode()
+    while current_char != '!':
+        len += current_char
+        current_char = my_socket.recv(1).decode()
+    return int(len)
+
 def time():
-    temp = datetime.datetime.now().strftime("%H:%M:%S")
-    return temp
+    hour = datetime.datetime.now().strftime("%H:%M:%S")
+    return str(hour)
+
+
 def name():
     return "daniel's server"
+
+
 def rand():
     num = random.randint(1, 10)
-    return num
+    return str(num)
+
+
+def return_value(msg):
+    if msg == "TIME":
+        return time()
+    elif msg == "NAME":
+        return name()
+    elif msg == "RAND":
+        return rand()
+
 
 
 def main():
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-
         my_socket.bind((IP, PORT))
-
         my_socket.listen(QUEUE_LEN)
 
         while True:
             client_socket, client_address = my_socket.accept()
 
             try:
-                check= True
+                check = True
                 while check:
-                    msg = client_socket.recv(MAX_PACKET).decode()
+                    msg = client_socket.recv(recieve_len_protocol(client_socket)).decode()
+                    print ("the message from client" + msg)
 
-
-                    if msg == "TIME":
-                        client_socket.send(str(time()).encode())
-                    elif msg == "NAME":
-                        client_socket.send(name().encode())
-                    elif msg == "RAND":
-                        client_socket.send(str(rand()).encode())
-                    elif msg == "EXIT":
-                        check = False
+                    if msg != "EXIT":
+                        client_socket.send(protocol_send(return_value(msg)).encode())
                     else:
-                        client_socket.send("not valid func".encode())
-
-
+                        check = False
 
             except socket.error as err:
 
                 print('received socket error on client socket' + str(err))
 
             finally:
-                print ("client left")
+                print("client left")
                 client_socket.close()
 
-
-
     except socket.error as err:
-
         print('received socket error on server socket' + str(err))
 
     finally:
